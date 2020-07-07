83605cc382ffc2099578d44d0c564ea5e31a27d2
zetcco <66688464+zetcco@users.noreply.github.com>
2020-07-07 20:16:01 +0530

    Add files via upload

---
+++ link_duplicate_remover.py
@@ -0,0 +1,18 @@
+outputLinks = []
+
+with open("links.txt", "r") as f:
+	inputLinks = f.readlines()
+
+removedLinks = len(inputLinks)
+print("Links removed: " + str(removedLinks))
+
+with open("output_links.txt", "w") as f:
+	for link in inputLinks:
+		if link not in outputLinks:
+			removedLinks -= 1
+			f.write(link)
+			outputLinks.append(link)
+
+print("Links removed: " + str(removedLinks))
+
+input()
